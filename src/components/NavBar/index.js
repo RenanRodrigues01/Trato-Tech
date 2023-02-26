a4c87a573e64ecb8fceb2672b23989850d52b6a8
renan <renan.rodriguesmoura2022@gmail.com>
2023-02-26 20:55:50 -0300

    criando componente de busca

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,6 +2,7 @@ import styles from './Navbar.module.scss';
 import { ReactComponent as Logo} from '../../assets/logo.svg';
 import classNames from 'classnames';
 import {RiShoppingCart2Line, RiShoppingCartFill} from 'react-icons/ri';
+import Busca from 'components/Busca';
 
 const iconesProp = {
   color: white,
@@ -9,29 +10,29 @@ const iconesProp = {
 }
 const NavBar = () => {
   return (
-   <nav className={styles.nav}>
-    <Logo className={styles.logo}/>
-    <div className={styles.links}>
-      <div>
-        <a href='/' className={classNames(styles.link, {
-          [styles.selected]: window.location.pathname === '/'
-        })}>
-          Pagina inicial
+    <nav className={styles.nav}>
+      <Logo className={styles.logo}/>
+      <div className={styles.links}>
+        <div>
+          <a href='/' className={classNames(styles.link, {
+            [styles.selected]: window.location.pathname === '/'
+          })}>
+            Pagina inicial
+          </a>
+        </div>   
+      </div>
+      <div className={styles.busca}>
+          <Busca />
+      </div>
+      <div className={styles.icones}>
+        <a href='/carrinho'>
+          {window.location.pathname === '/carrinho'
+            ? <RiShoppingCartFill {...iconesProp} />
+            : <RiShoppingCart2Line {...iconesProp} />
+          }
         </a>
-      </div>   
-    </div>
-    <div className={styles.busca}>
-
-    </div>
-    <div className={styles.icones}>
-      <a href='/carrinho'>
-        {window.location.pathname === '/carrinho'
-          ? <RiShoppingCartFill {...iconesProp} />
-          : <RiShoppingCart2Line {...iconesProp} />
-        }
-      </a>
-    </div>
-   </nav>
+      </div>
+    </nav>
   )
 }
 
