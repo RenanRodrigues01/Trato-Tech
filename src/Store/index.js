b46a3b1bd5b2fbe3c9abe28cb563dbb44b3dfe25
renan <renan.rodriguesmoura2022@gmail.com>
2023-02-27 16:33:04 -0300

    criando reducer para itens

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,9 +1,11 @@
 import {  configureStore } from '@reduxjs/toolkit';
 import categoriasSlice from './Reducers/categorias';
+import itensSlice from './Reducers/itens';
 
 const store = configureStore({
     reducer: {
-        categorias: categoriasSlice
+        categorias: categoriasSlice,
+        itens: itensSlice,
     }
 });
 
