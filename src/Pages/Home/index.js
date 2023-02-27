1fbab4a999da08bb4dfeed4d1cc7a7c383ded788
renan <renan.rodriguesmoura2022@gmail.com>
2023-02-27 11:26:12 -0300

    implementando home da aplicação

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,9 +1,16 @@
-import styles from './Home.module.scss'
+import Header from 'components/Header';
+import styles from './Home.module.scss';
+import Relogio from 'assets/inicial.png'
 
 const Home = () => {
   return (
     <div>
-      
+      <Header
+        titulo='Classificados Tech'
+        descricao='Compre diversos tipos de produto no melhor site do Brasil!'
+        className={styles.header}
+        imagem={Relogio}
+      />
     </div>
   )
 }
