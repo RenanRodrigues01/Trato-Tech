06f422e38dfe6bd08e2937792cb8af88568e186a
renan <renan.rodriguesmoura2022@gmail.com>
2023-03-02 15:30:23 -0300

    controlado input, e zerando quando ocorre uma mudança de pagina

--- src/components/Busca/index.js
+++ src/components/Busca/index.js
@@ -1,11 +1,24 @@
-import styles from './Busca.module.scss'
+import styles from './Busca.module.scss';
+import { useSelector, useDispatch } from 'react-redux';
+import { mudarBusca, resetarBusca } from 'Store/Reducers/busca';
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const Busca = () => {
+  const busca = useSelector(state => state.busca);
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  useEffect(() => {
+    dispatch(resetarBusca())
+  }, [location.pathname, dispatch])
   return (
     <div className={styles.busca}>
         <input
-            className={styles.input}
-            placeholder = "O que você procura?"
+          className={styles.input}
+          placeholder = "O que você procura?"
+          value={busca}
+          onChange={e => dispatch(mudarBusca(e.target.value))}
         /> 
     </div>
   )
