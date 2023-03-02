5d52627f4f1692023aa5af46da501459398d39e7
renan <renan.rodriguesmoura2022@gmail.com>
2023-03-02 15:22:22 -0300

    criando reducer responsavel pela busca

--- src/Store/index.js
+++ src/Store/index.js
@@ -2,12 +2,14 @@ import {  configureStore } from '@reduxjs/toolkit';
 import categoriasSlice from './Reducers/categorias';
 import itensSlice from './Reducers/itens';
 import carrinhoSlice from './Reducers/carrinho';
+import buscaSlice from './Reducers/busca';
 
 const store = configureStore({
     reducer: {
         categorias: categoriasSlice,
         itens: itensSlice,
         carrinho: carrinhoSlice,
+        busca: buscaSlice
     }
 });
 
